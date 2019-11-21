12519bd3a3ad347d60eac8eb297520a21436b40d
Javier Elio <javiereliomedina@gmail.com>
2019-11-21 14:07:27 +0100

    change fontsize to 11pt

--- CV_Table.R
+++ CV_Table.R
@@ -5,6 +5,6 @@ CV_Table <-  function(df) {
     kable(col.names = NULL, escape = FALSE, align = 1L, longtable = TRUE) %>%
     kableExtra::kable_styling(full_width = F) %>%
     kableExtra::column_spec(1, color = "darkgray", width = "3.5em") %>%
-    kableExtra::column_spec(2, width = "37em")
+    kableExtra::column_spec(2, width = "40.5em")
   
 }
