974daa5880397186282823f9cb4000dfa6d0ce23
Javier Elio <javiereliomedina@gmail.com>
2021-08-16 16:53:55 +0200

    Update aux. functions for cleaning ORCID data

--- CV/Format_ORCID.R
+++ CV/Format_ORCID.R
@@ -1,8 +1,8 @@
 # Format information from ORCID
 
 
-# Function 1 for cleaning:  Education, employment, Invited Positions, service
-format_ORCID_1 <- function(x) {
+# Function 1 for cleaning:  Education
+format_ORCID_edu <- function(x) {
   
   x %>% 
     pluck("affiliation-group", "summaries") %>% 
@@ -31,9 +31,39 @@ format_ORCID_1 <- function(x) {
   
 }
 
+# Function 2 for cleaning:  Employment, Invited Positions, Service
+format_ORCID_eis <- function(x) {
+  
+  x %>% 
+    pluck("affiliation-group", "summaries") %>% 
+    bind_rows() %>% 
+    select(contains(c("start-date.year",
+                      "end-date.year",
+                      "role-title",
+                      "organization.name",
+                      "department-name",
+                      "city",
+                      "country"))) %>% 
+    janitor::remove_empty(which = "cols") %>% 
+    rename(Year_Start = contains("start-date.year"),
+           Year_End   = contains("end-date.year"),
+           Title      = contains("role-title"),
+           Uni        = contains("organization.name"),
+           Depr       = contains("department-name"),
+           City       = contains("city"),
+           Country    = contains("country")) %>% 
+    mutate(Year_End = str_sub(Year_End, -2), 
+           Title = bold_text(Title)) %>%
+    unite(Year, Year_Start, Year_End, sep = "-", na.rm = TRUE) %>% 
+    unite(Details, Title, Uni, Depr, City, Country, sep = ", ", na.rm = TRUE) %>%
+    select(Year, Details) %>%
+    arrange(desc(Year))
+  
+}
 
-# Function 2 for cleaning: Memberships
-format_ORCID_2 <- function(x) {
+
+# Function 3 for cleaning: Memberships
+format_ORCID_memb <- function(x) {
   x %>% 
     pluck("affiliation-group", "summaries") %>% 
     bind_rows() %>% 
@@ -57,4 +87,3 @@ format_ORCID_2 <- function(x) {
     select(Year, Details) %>%
     arrange(desc(Year))
 }
-
